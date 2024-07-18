86caeeb970f2e2e3f1478434a9bc35f01ddd16b9
Oliver Hughes <oliver.hughes@adevinta.com>
2024-07-19 01:50:38 +0200

    update experience

--- view/page_index.go
+++ view/page_index.go
@@ -74,11 +74,13 @@ func (s *Static) pageIndex(tmpl *template.Template) {
 				Title: "DevOps",
 				Color: "amber",
 				Items: []item{
+					{Label: "Git", Badge: "{ }"},
 					{Label: "Amazon Web Services", Badge: "AWS"},
+					{Label: "Linux Administation", Badge: "/"},
+					{Label: "Windows Server", Badge: "C:\\"},
 					{Label: "Github Actions", Badge: "GHA"},
 					{Label: "Travis", Badge: "{ }"},
 					{Label: "Docker", Badge: "{ }"},
-					{Label: "Kubernetes", Badge: "{ }"},
 				},
 			},
 		},
@@ -89,7 +91,7 @@ func (s *Static) pageIndex(tmpl *template.Template) {
 				Date:     "2019 - Present",
 				Location: "Barcelona, Spain",
 				Description: []string{
-					"Leading the frontend initiative to evolve and build the in-house built experimentation platform.",
+					"Leading the frontend initiative to evolve and build the in-house built experimentation platform and libraries used across European marketplaces with millions of users.",
 				},
 			}, {
 				Title:    "Web Developer",
@@ -100,13 +102,21 @@ func (s *Static) pageIndex(tmpl *template.Template) {
 					"Working on a variety of projects from small brochure sites to large scale web applications. As well, managing the hosting infrastructure for the company and implementing continuous deployment and testing methodologies.",
 				},
 			},
-			{
-				Title:    "Head of Disaster Recovery",
+			{Title: "Head of Disaster Recovery",
 				Company:  "UKFast",
-				Date:     "2013 - 2017",
+				Date:     "2015 - 2017",
 				Location: "Manchester, UK",
 				Description: []string{
-					"Joined as a Windows Server engineer and quickly moved into the Disaster Recovery team. I was responsible for the design and implementation of the disaster recovery solutions for the company's clients. I also managed the team and the day to day operations of the department.",
+					"Moving from the Senior Windows team to take lead of the Disaster Recovery team, I was responsible for the design and implementation of the disaster recovery solutions for the company's enterprise clients, as well as architecting and scaling the backup infrastructure to handle petabytes of data distibuted across multiple datacenters. I also managed the team and the day to day operations of the department.",
+				},
+			}, {
+				Title:    "Windows Server Engineer",
+				Company:  "UKFast",
+				Date:     "2013 - 2015",
+				Location: "Manchester, UK",
+				Description: []string{
+					"Starting in the company as a Windows Engineer, day to day duties comprised of supporting customers via phone or ticket system for their solutions hosted with UK Fast. This included but not limited to support with Windows 2008/2012, Active Directory, Exchange, IIS, VMWare, Hyper-V, MSSQL and basic support on CentOS, Red Hat and Ubuntu servers.",
+					"Within a few months I was then promoted to a Senior Windows Engineer, which involved supporting customers of a higher tier with more complex solutions such as clusters, DAGs, MSSQL High Availability and multi node Exchange environments.",
 				},
 			}, {
 				Title:    "Technical Support Analyst",
@@ -114,7 +124,7 @@ func (s *Static) pageIndex(tmpl *template.Template) {
 				Date:     "2008 - 2013",
 				Location: "Manchester, UK",
 				Description: []string{
-					"Providing 1st to 3rd line technical support.",
+					"Providing 1st to 3rd line technical support to a user base of 500+. Daily activities ranged widely from repairing laptops, software troubleshooting remotely or face to face, to server administration, managing Active Directory and Exchange servers.",
 				},
 			},
 		},
