08f10de1e5b2e62af187178cf2e48a59de004b33
Oliver Hughes <hughes.oliver88@gmail.com>
2025-03-04 23:21:17 +0100

    Add fade in effect to skills

--- public/main.js
+++ public/main.js
@@ -39,3 +39,39 @@
     localStorage.setItem("theme", isDark ? "dark" : "light");
   });
 })();
+
+(function registerFadeInElements() {
+  const elements = document.querySelectorAll("[data-fade-in]");
+  if (elements.length === 0) return;
+  const staggerDelay = 150;
+  const animateEntry = (entry, index) => {
+    const element = entry.target;
+    if (element.dataset.animated) {
+      return;
+    }
+    if (!entry.isIntersecting) {
+      element.classList.add("opacity-10");
+      element.classList.add("translate-y-10");
+      return;
+    }
+    element.dataset.animated = "true";
+    setTimeout(() => {
+      element.classList.remove("opacity-10");
+      element.classList.remove("translate-y-10");
+    }, index * staggerDelay);
+  };
+  const intersectionObserver = new IntersectionObserver(
+    (entries) => {
+      entries.forEach(animateEntry);
+    },
+    {
+      root: null,
+      threshold: 0.1,
+    },
+  );
+  elements.forEach((element) => {
+    element.classList.add("transition");
+    element.classList.add("duration-800");
+    intersectionObserver.observe(element);
+  });
+})();
